6f0f364af51204db4b6b10b61430df5b22dab414
dntsdnts <17964609+dntsdnts@users.noreply.github.com>
2024-11-01 23:39:39 +0900

    functions

--- functions/run-as-backup.fish
+++ functions/run-as-backup.fish
@@ -3,5 +3,5 @@ function run-as-backup
     or return 1
     set -l _id (adb shell pm list packages -U|rg $argv|rg -o \\d+\$)
     adb install-multiple -i @null\n"qrqr $_id 1 /data/user/0/$argv/ default:targetSdkVersion=28 none 0 0 1 @null" (adb shell pm list packages -f|rg $argv|rg -o /data/.\*/)*.apk
-    adb shell run-as qrqr tar -c data/user/0/$argv >>$argv.tar
+    adb shell run-as qrqr tar -c . >$argv.appdata
 end
