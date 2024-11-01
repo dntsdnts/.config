eae8736f78b9304a06a52930e5eb29925dcc0aa7
dntsdnts <17964609+dntsdnts@users.noreply.github.com>
2024-11-01 23:39:39 +0900

    functions

--- functions/run-as-restore.fish
+++ functions/run-as-restore.fish
@@ -3,5 +3,5 @@ function run-as-restore
     or return 1
     set -l _id (adb shell pm list packages -U|rg $argv|rg -o \\d+\$)
     adb install-multiple -i @null\n"qrqr $_id 1 /data/user/0/$argv/ default:targetSdkVersion=28 none 0 0 1 @null" (adb shell pm list packages -f|rg $argv|rg -o /data/.\*/)*.apk
-    adb shell run-as qrqr tar -x <$argv.tar
+    adb shell run-as qrqr tar -x <$argv.appdata
 end
