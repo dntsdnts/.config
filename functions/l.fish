f0d016a8efdb7ba7f0bda65c35c69f70b9226ca9
dntsdnts <17964609+dntsdnts@users.noreply.github.com>
2025-01-04 02:06:32 +0800

    functions

---
+++ functions/l.fish
@@ -0,0 +1,3 @@
+function l --wraps='eza -F' --wraps='ls -F' --description 'ls -F'
+    ls -F $argv
+end
--- functions/run-as-backup.fish
+++ functions/run-as-backup.fish
@@ -3,7 +3,7 @@ function run-as-backup
     or return 1
     set -l _id (adb shell pm list packages -U|rg $argv|rg -o \\d+\$)
     ##adb shell pm uninstall --user 0 com.miui.securitycenter
-    adb install-multiple -i @null\n"qrqr $_id 1 /data/user/0/$argv default:targetSdkVersion=28 none 0 0 1 @null" base.apk
+    adb install-multiple -i @null\n"qrqr $_id 1 /data/user/0/$argv/ default:targetSdkVersion=28 none 0 0 1 @null" base.apk
     ##adb shell pm install-existing --user 0 com.miui.securitycenter
     adb shell run-as qrqr tar -c . >$argv.appdata
 end
