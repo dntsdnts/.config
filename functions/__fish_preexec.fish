f0ac9f480b40e1efa35b5b1dcd53a7cb08191633
dntsdnts <17964609+dntsdnts@users.noreply.github.com>
2025-01-28 02:45:35 +0800

    functions

---
+++ functions/__fish_preexec.fish
@@ -0,0 +1,3 @@
+function __fish_preexec --on-event fish_preexec
+    set -e LD_LIBRARY_PATH
+end
--- functions/run-as-restore.fish
+++ functions/run-as-restore.fish
@@ -5,5 +5,6 @@ function run-as-restore
     ##adb shell pm uninstall --user 0 com.miui.securitycenter
     adb install-multiple -i @null\n"qrqr $_id 1 /data/user/0/$argv default:targetSdkVersion=28 none 0 0 1 @null" base.apk
     ##adb shell pm install-existing --user 0 com.miui.securitycenter
+    adb
     adb shell run-as qrqr tar -x <$argv.appdata
 end
