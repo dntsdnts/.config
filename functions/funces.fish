5bd54e21dc86f62dce21f3475a8f5d2ae72dc090
dntsdnts <17964609+dntsdnts@users.noreply.github.com>
2025-12-15 03:16:39 +0800

    func

--- functions/funces.fish
+++ functions/funces.fish
@@ -1,4 +1,13 @@
 function funces --wraps=funced\ \$argv\nfuncsave\ \$argv\[1\] --description 'alias funced&&funcsave'
     EDITOR=fish funced $argv
+    and begin
+        set -l b \e\[1\;5m
+        set -l n \e\[0m
+        switch (read -l -n1 -P write\ $b$argv[1]$n?[𝐘/n])
+            case '' y Y
+            case '*'
+                return -1
+        end
+    end
     and funcsave $argv[1]
 end
--- functions/gitcd.fish
+++ functions/gitcd.fish
@@ -1,5 +1,5 @@
-function gitcd
-    git clone $argv[1] $argv[2]
-    if set -S argv[2]; and $argv[2]/; or cd (path basename $argv[1])
+function gitcd --wraps='git clone $argv[1] $argv[2]'
+    git clone $argv[1] $argv[2] $argv[3..]
+    and if test ! -z $argv[2]; and $argv[2]/; or cd (path basename $argv[1])
     end
 end
