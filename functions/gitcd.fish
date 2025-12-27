2ccbd7198cbfc178c7685f1da2d667bd7135305d
dntsdnts <17964609+dntsdnts@users.noreply.github.com>
2025-12-27 19:03:33 +0800

    func

--- functions/gitcd.fish
+++ functions/gitcd.fish
@@ -1,5 +1,8 @@
 function gitcd --wraps='git clone $argv[1] $argv[2]'
     git clone $argv[1] $argv[2] $argv[3..]
-    and if test ! -z $argv[2]; and $argv[2]/; or cd (path basename $argv[1])
+    and if test ! -z "$argv[2]"
+        cd $argv[2]
+    else
+        cd (basename -s .git $argv[1])
     end
 end
