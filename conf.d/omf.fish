6622a06383fd457761d9511721847327a280fae9
qrqr <17964609+dntsdnts@users.noreply.github.com>
2026-06-19 17:20:30 +0800

    do clean

--- conf.d/omf.fish
+++ conf.d/omf.fish
@@ -1,3 +1,5 @@
+set -q OMF_PATH
+and begin
 # Path to Oh My Fish install.
 set -q XDG_DATA_HOME
 and set -gx OMF_PATH "$XDG_DATA_HOME/omf"
@@ -5,3 +7,4 @@ or set -gx OMF_PATH "$HOME/.local/share/omf"
 
 # Load Oh My Fish configuration.
 source $OMF_PATH/init.fish
+end
